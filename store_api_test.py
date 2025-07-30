0be79851f97ca7c242b2dda0a580b7f8fd9c2b40
RoeeAsras1 <roeeas084@gmail.com>
2025-07-30 16:19:06 +0300

    Update store_api_test.py

--- store_api_test.py
+++ store_api_test.py
@@ -1,3 +1,8 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
 def test_contact_us_valid():
     driver = webdriver.Chrome()
     driver.maximize_window()
